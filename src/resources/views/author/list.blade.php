73315bb09908d86bbefaf1d5d840e1a7c9506158
Davids-Adamovics <davidsadamovicss@gmail.com>
2024-04-19 18:09:09 +0300

    fix

--- src/resources/views/author/list.blade.php
+++ src/resources/views/author/list.blade.php
@@ -1,36 +1,36 @@
 @extends('layout')
- 
+
 @section('content')
- 
-    <h1>{{ $title }}</h1>
- 
-    @if (count($items) > 0)
- 
-        <table class="table table-striped table-hover table-sm">
-            <thead class="thead-light">
-                <tr>
-                    <th>ID</td>
-                    <th>Vārds</td>
-                    <th>&nbsp;</td>
-                </tr>
-            </thead>
-            <tbody>
- 
-            @foreach($items as $author)
-            <tr>
-                <td>{{ $author->id }}</td>
-                <td>{{ $author->name }}</td>
-                <td>Labot / Dzēst</td>
-            </tr>
-            @endforeach
- 
-            </tbody>
-        </table>
- 
-    @else
- 
-        
- 
-    @endif
- 
+
+<h1>{{ $title }}</h1>
+
+@if (count($items) > 0)
+
+<table class="table table-striped table-hover table-sm">
+    <thead class="thead-light">
+        <tr>
+            <th>ID</td>
+            <th>Vārds</td>
+            <th>&nbsp;</td>
+        </tr>
+    </thead>
+    <tbody>
+
+        @foreach($items as $author)
+        <tr>
+            <td>{{ $author->id }}</td>
+            <td>{{ $author->name }}</td>
+            <td>Labot / Dzēst</td>
+        </tr>
+        @endforeach
+
+    </tbody>
+</table>
+
+@else
+
+
+
+@endif
+<a href="/authors/create" class="btn btn-primary">Izveidot jaunu</a>
 @endsection
